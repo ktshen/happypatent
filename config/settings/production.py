6ef2a4c2c9e5aa7898ae46bd2a7d9254d7185eca
ktshen <bruce20036@gmail.com>
2017-06-03 13:25:10 +0800

    Add django compressor settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -157,6 +157,11 @@ LOGGING = {
     }
 }
 
+# DJANGO COMPRESSOR
+COMPRESS_ENABLED = True
+COMPRESS_CSS_FILTERS = ['compressor.filters.css_default.CssAbsoluteFilter',  'compressor.filters.cssmin.rCSSMinFilter']
+COMPRESS_JS_FILTERS = ['compressor.filters.jsmin.JSMinFilter', 'compressor.filters.jsmin.SlimItFilter']
+
 # Custom Admin URL, use {% url 'admin:index' %}
 ADMIN_URL = env('DJANGO_ADMIN_URL')
 
