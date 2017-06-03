aadc11b7971727130fa872f1547dde02c5602d0e
ktshen <bruce20036@gmail.com>
2017-06-03 14:42:41 +0800

    Fix bugs

--- compose/django/gunicorn.sh
+++ compose/django/gunicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python /app/manage.py collectstatic --noinput
-/usr/local/bin/gunicorn config.wsgi -w 2 -b unix:/app/media/gunicorn.sock -k gevent --chdir=/app --access-logfile -
+/usr/local/bin/gunicorn config.wsgi -w 2 -b unix:/app/media/gunicorn.sock --worker-class=gevent --chdir=/app --access-logfile -
