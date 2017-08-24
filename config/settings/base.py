f0f9d1ba9f445bd1bbdd6ce344e653b9ae20a816
ktshen <bruce20036@gmail.com>
2017-08-24 23:08:51 +0800

    Remove watson

--- config/settings/base.py
+++ config/settings/base.py
@@ -83,7 +83,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'watson.middleware.SearchContextMiddleware',
 ]
 
 # MIGRATIONS CONFIGURATION
